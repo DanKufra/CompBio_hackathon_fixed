d7d50cc9349135057b60df4ba2c86d9fc5bf4b10
GuyA <guy.amir2@mail.huji.ac.il>
2020-01-21 09:29:16 +0200

    1

--- HMM2_stats.py
+++ HMM2_stats.py
@@ -2,12 +2,20 @@ import emission_motif
 import our_gen
 import transition_motifs
 import markov_model_runner
+import pandas as pd
 
-#transition_motifs.create_trans(15,1,15,15,1,15)
+# transition_motifs.create_trans(15,1,15,15,1,15)
 
-for i in range(1,16):
-    print(i)
-    transition_motifs.create_trans(i,1,i,i,1,i)
-    emission_motif.create_emiss(i,1,i,i,1,i)
+# for i in range(1, 16):
+    #transition_motifs.create_trans(i, 1, i, i, 1, i)
+    #emission_motif.create_emiss(i, 1, i, i, 1, i)
+
+    emission_df = pd.read_csv("emi.tsv", sep=' ', index_col=0)
+    transition_df = pd.read_csv("trans.tsv", sep=' ', index_col=0)
+
     gen = our_gen.getSeq("hg19.genes.NR.chr19.exonCount2_29.bed", "hg19.2bit")
-    loss = markov_model_runner("trans.tsv","emi.tsv",'')
+    loss = markov_model_runner.run_and_stat(emission_df=emission_df, transition_df=transition_df, start_state="d",
+                                            generator=gen,pos_vals=['a','b','c'], neg_vals=['d','e','f'])
+
+
+
